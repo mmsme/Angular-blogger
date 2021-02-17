7ca594fed321b2940dc3a99325c711e09d9029b7
mmsme <mmsmefcis@gmail.com>
2021-02-17 16:38:51 +0200

    color

--- src/app/services/img-div.service.ts
+++ src/app/services/img-div.service.ts
@@ -4,18 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ImgDivService {
-  colors = [
-    '#374045',
-    '#008891',
-    '#ff4646',
-    '#272343',
-    '#222831',
-    '#fdb827',
-    '#351f39',
-    '#54e346',
-    '#204051',
-    '#007965',
-  ];
+
+
 
   constructor() {}
 
@@ -23,8 +13,39 @@ export class ImgDivService {
     return name?.charAt(0).toUpperCase();
   }
 
-  chooseColor(): string {
-    return this.colors[Math.floor(Math.random() * 10)];
+  chooseColor(name: string): any {
+    let color;
+    var key = name?.toLowerCase().charAt(0);
+    switch(key)  {
+      case 'a':  { color = '#374045'; break;}
+      case 'b':  { color = '#222831'; break;}
+      case 'c':  { color = '#ff4646'; break;}
+      case 'd':  { color = '#272343'; break;}
+      case 'e':  { color = '#008891'; break;}
+      case 'f':  { color = '#fdb827'; break;}
+      case 'g':  { color = '#351f39'; break;}
+      case 'h':  { color = '#54e346'; break;}
+      case 'i':  { color = '#204051'; break;}
+      case 'j':  { color = '#007965'; break;}
+      case 'k':  { color = '#1e212d'; break;}
+      case 'l':  { color = '#0a043c'; break;}
+      case 'm':  { color = '#0f1123'; break;}
+      case 'n':  { color = '#153e90'; break;}
+      case 'o':  { color = '#fecd1a'; break;}
+      case 'p':  { color = '#790c5a'; break;}
+      case 'q':  { color = '#01c5c4'; break;}
+      case 'r':  { color = '#5d54a4'; break;}
+      case 's':  { color = '#822659'; break;}
+      case 't':  { color = '#4d375d'; break;}
+      case 'u':  { color = '#16c79a'; break;}
+      case 'v':  { color = '#c70039'; break;}
+      case 'w':  { color = '#ca431d'; break;}
+      case 'x':  { color = '#ff5722'; break;}
+      case 'y':  { color = '#cdb30c'; break;}
+      case 'z':  { color = '#d2e603'; break;}
+    };
+
+    return color;
   }
 
   nameToCaptial(name: string): string {
